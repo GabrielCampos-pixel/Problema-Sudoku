2bab81d469ea3f98f7f34297436be9f5112dfc85
GabrielCampos <gabrielfilipecampos1@gmail.com>
2026-06-17 13:05:25 +0000

    removendo codigo desnecessario

--- Sudoku.java
+++ Sudoku.java
@@ -1,9 +1,7 @@
 public class Sudoku {
-    private int N;              // Tamanho do tabuleiro (4, 9 ou 16)
-    private int blockSize;      // Tamanho do bloco (2, 3 ou 4)
+    private int N;              // Tamanho do tabuleiro (9 ou 16)
+    private int blockSize;      // Tamanho do bloco (3 ou 4)
 
-    private long backtracksMRV = 0;
-    private long backtracksColoracao = 0;
     private long recursividadeMRV = 0;
     private long recursividadeColoracao = 0;
 
@@ -33,13 +31,16 @@ public class Sudoku {
 
     private int[] encontrarMelhorCelula(int[][] tabuleiro) {
         int minOpcoes = Integer.MAX_VALUE;
-        int melhorLinha = -1, melhorColuna = -1;
-
+        int melhorLinha = -1;
+        int melhorColuna = -1;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (tabuleiro[i][j] == 0) {
                     int opcoes = contarOpcoes(tabuleiro, i, j);
-                    if (opcoes < minOpcoes && opcoes > 0) {
+                    if (opcoes == 0) {
+                        return new int[]{i, j};
+                    }
+                    if (opcoes < minOpcoes) {
                         minOpcoes = opcoes;
                         melhorLinha = i;
                         melhorColuna = j;
@@ -60,21 +61,18 @@ public class Sudoku {
 
     private boolean resolverComHeuristica(int[][] tabuleiro) {
         recursividadeMRV++;
-        int[] celula = encontrarMelhorCelula(tabuleiro);//divisao e conquista,comeca escolhendo um subproblema
+        int[] celula = encontrarMelhorCelula(tabuleiro);
         int linha = celula[0];
         int coluna = celula[1];
-        if (linha == -1) return true;//problema resolvido no caso base
-        for (int num = 1; num <= N; num++) {//teste da divisao com cada possibilidade
+        if (linha == -1) return true;
+        for (int num = 1; num <= N; num++) {
             if (ehSeguro(tabuleiro, linha, coluna, num)) {
-                tabuleiro[linha][coluna] = num;//realmente ocorre a divisao
-                if (resolverComHeuristica(tabuleiro)) {//chamada recursiva para resolver o subproblema com 1 celula a menos
-                    return true;//caso a conquista tenha sucesso
-                }    
+                tabuleiro[linha][coluna] = num;
+                if (resolverComHeuristica(tabuleiro)) return true;
                 tabuleiro[linha][coluna] = 0;
-                backtracksMRV++;//caso a conquista falhe retrocede para fazer novamente com outra celula
             }
         }
-        return false;//caso o caminho nao tenha solucao
+        return false;
     }
 
     private boolean resolverColoracaoGrafos(int[][] tabuleiro) {
@@ -82,15 +80,12 @@ public class Sudoku {
         int[] celula = encontrarMelhorCelula(tabuleiro);
         int linha = celula[0];
         int coluna = celula[1];
-
         if (linha == -1) return true;
-
         for (int cor = 1; cor <= N; cor++) {
             if (ehSeguro(tabuleiro, linha, coluna, cor)) {
                 tabuleiro[linha][coluna] = cor;
                 if (resolverColoracaoGrafos(tabuleiro)) return true;
                 tabuleiro[linha][coluna] = 0;
-                backtracksColoracao++;
             }
         }
         return false;
@@ -106,13 +101,13 @@ public class Sudoku {
     }
 
     private void imprimirEstatisticas() {
-        System.out.println("=== Estatísticas ===");
-        System.out.println("Heurística MRV foi utilizado: Backtracks: " + backtracksMRV + " | Recursoes: " + recursividadeMRV);
-        System.out.println("Coloraçao + Heuristica foi utilizado: Backtracks: " + backtracksColoracao + " | Recursoes: " + recursividadeColoracao);
+        System.out.println("Estatisticas");
+        System.out.println("Heuristica MRV foi utilizado: Recursoes: " + recursividadeMRV);
+        System.out.println("Coloraçao + Heuristica foi utilizado: Recursoes: " + recursividadeColoracao);
     }
 
     public static void main(String[] args) {
-        int[] tamanhos = {4, 9, 16};
+        int[] tamanhos = {9, 16};
         for (int tam : tamanhos) {
             System.out.println("Sudoku " + tam + " x " + tam);
             Sudoku sudoku = new Sudoku(tam);
@@ -123,28 +118,21 @@ public class Sudoku {
             long inicio = System.currentTimeMillis();
             boolean resolvidoMRV = sudoku.resolverComHeuristica(copia1);
             long tempoMRV = System.currentTimeMillis() - inicio;
-            System.out.println("\n Resolvido com Heuristica MRV em " + tempoMRV + " ms");
+            System.out.println("\n Resolvido com Heurística MRV em " + tempoMRV + " ms");
             sudoku.imprimirTabuleiro(copia1);
             sudoku = new Sudoku(tam);
             int[][] copia2 = copiarTabuleiro(tabuleiro);
             inicio = System.currentTimeMillis();
             boolean resolvidoGrafos = sudoku.resolverColoracaoGrafos(copia2);
             long tempoGrafos = System.currentTimeMillis() - inicio;
-            System.out.println("\n Resolvido com Coloraçao de Grafos + Heuristica em " + tempoGrafos + " ms");
+            System.out.println("\n Resolvido com Coloração + Heurística em " + tempoGrafos + " ms");
             sudoku.imprimirTabuleiro(copia2);
             sudoku.imprimirEstatisticas();
         }
     }
 
     private static int[][] gerarTabuleiroExemplo(int tam) {
-        if (tam == 4) {
-            return new int[][]{
-                {0, 0, 0, 2},
-                {0, 3, 0, 0},
-                {1, 0, 4, 0},
-                {0, 0, 0, 3}
-            };
-        } else if (tam == 9) {
+        if (tam == 9) {
             return new int[][]{
                 {5, 3, 0, 0, 7, 0, 0, 0, 0},
                 {6, 0, 0, 1, 9, 5, 0, 0, 0},
